acf63981d3abdbcf9f7cdeddeae2bc4c15703928
Fernando <Fernando>
2024-04-29 08:54:26 +0200

    Refactorización hecho

--- src/pullup/Car.java
+++ src/pullup/Car.java
@@ -2,8 +2,6 @@ package pullup;
 
 public class Car extends Vehicle {
 	private String matricula;
-	private String maletero;
-	private boolean isMaleteroAbierto;
 	
 	public void start() {
 	}
--- src/pullup/Vehicle.java
+++ src/pullup/Vehicle.java
@@ -3,4 +3,8 @@ package pullup;
 
 public class Vehicle {
 	protected String nombre;
+    protected String casco;
+    protected String matricula;
+    protected boolean isMaleteroAbierto;
+    protected String maletero;
 }
